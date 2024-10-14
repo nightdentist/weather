af814641fa695217226dbfae73b94bac0f4d7fb6
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-10-15 02:12:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+url_template = 'https://wttr.in/{}?m?M?n?qTqu&lang=ru'
+url = url_template.format('london').format('cherepovec')
+response = requests.get(url)
+response.raise_for_status()
+print(response.text)
+
+url = url_template.format('cherepovec')
+response = requests.get(url)
+response.raise_for_status()
+print(response.text)
+
+url = url_template.format('sheremetievo')
+response = requests.get(url)
+response.raise_for_status()
+print(response.text)
