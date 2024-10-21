a0a66f77f2c675a55e11ad9fbf81b744313d7a99
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-10-21 22:24:46 +0300

    next_commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 
-url_template = 'https://wttr.in/{}?m?M?n?qTqu&lang=ru'
-citys = 'london', 'cherepovec', 'sheremetievo'
 
-for city in citys:
+payload = {'?m': '', '?M': '', '?n': '', '?q': '', 'Tqu&lang=ru': ''}
+url_template = ('https://wttr.in/{}')
+cities = 'london', 'cherepovec', 'sheremetievo'
+
+for city in cities:
     url = url_template.format(city)
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     print(response.text)
